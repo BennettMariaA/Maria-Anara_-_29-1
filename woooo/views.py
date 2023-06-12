0c1eb6d8f1302a363a09a91a3b2feb665b710691
BennettMariaA <maria06mb@gmail.com>
2023-06-12 23:05:44 +0600

    first commit

---
+++ woooo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Product
+
+def main_view(request):
+    return render(request, 'layouts/main.html')
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'products/products.html', {'products': products})
+from django.shortcuts import render
+
